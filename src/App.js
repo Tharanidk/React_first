668a21f89ab8e6dc03f069c566b4320b23a6595e
Tharani <tharani.dayadhi@gmail.com>
2020-04-17 16:15:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Create from './Components/create.component';
+import Edit from './Components/edit.component';
+import Index from './Components/index.component';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand"> REACT CURD example</Link>
+            <div className="collapse navbar=collapse" id="navbarSupportedContent" ></div>
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={'/'} className="nav-Link">Home &nbsp; </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/create'} className="nav-Link">Create &nbsp;</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/edit'} className="nav-Link">Edit &nbsp; </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/index'} className="nav-Link">Index &nbsp;</Link>
+              </li>
+            </ul>
+          </nav>
+          <h2>Welcome to English Quick Pack</h2>
+          <Switch>
+            <Route exact path='/create' component={Create} />
+            <Route exact path='/edit' component={Edit} />
+            <Route exact path='/index' component={Index} />
+          </Switch>
+
+        </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Components/create.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class Create extends Component {
+    render() {
+        return (
+            <div style={{ marginTop: 10 }}>
+                <h3>
+                    Add New Business
+                </h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Person Name</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Business Name</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add NIC Number</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Register Business" className="btn btn-primary"></input>
+                    </div>
+                </form>
+            </div>
+
+        );
+
+    }
+}
